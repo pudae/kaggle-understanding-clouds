1aac563f609011112e4388b9f02ecb44bcda93a0
pudae <pudae81@gmail.com>
2019-11-22 01:43:14 +0000

    Add scripts for downloading pretraind models

---
+++ download_pretrained.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_a_00.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_a_01.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_a_02.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_00.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_01.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_02.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_03.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_04.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_05.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_06.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_07.pth -p checkpoints
+kaggle datasets download pudae81/understandingclouds1stplaceweights -f model_b_08.pth -p checkpoints
